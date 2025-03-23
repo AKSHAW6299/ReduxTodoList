7c042d7d584755d940d0fccec868058c138511e5
Anand6262 <pyexpert62@gmail.com>
2025-03-24 01:24:30 +0530

    dev tool

--- src/store.js
+++ src/store.js
@@ -3,6 +3,9 @@ import rootReducer from "./reducer/index";
 
 
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// We added the below line to use react dev tool extension
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 export default store;
